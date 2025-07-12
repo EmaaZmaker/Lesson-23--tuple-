4bbf59ec6afd5c2462c7ab46a6dc35be694e511b
EmaaZmaker <emaazbinammar@gmail.com>
2025-07-12 10:27:01 +0400

    nfcewhjdfkn

---
+++ activity1.py
@@ -0,0 +1,10 @@
+tuple1=(1,"a",True)
+print(tuple1)
+tupleint=(1,5,3,6,3)
+print(tupleint)
+newtuple=tupleint+(9,)
+print(newtuple)
+#Tuples are immutable,thats why we did concatenation
+print(tupleint.count(3))
+newint=(3,4,6,2,7,8,0)
+print(newint[1:6])
